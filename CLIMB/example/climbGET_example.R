c7aaa4791812a143c85e833c41d8a3a1b4ee540f
Narges Rezaie <nargesr@uci.edu>
2024-11-06 15:38:39 -0800

    update climb pipeline

--- CLIMB/example/climbGET_example.R
+++ CLIMB/example/climbGET_example.R
@@ -1,12 +1,84 @@
 library(tidyverse)
 source("../source/climbGET.R")
 
-fqf_key <- read_csv("fastqfiles_key.csv")
+ID = c("13436",
+            "13438",
+            "13426",
+            "13427",
+            "13430",
+            "13431",
+            "13424",
+            "13421",
+            "13422",
+            "13416")
+
+gts <- climbGET(ID, facet="genotypes", "animalID") %>%
+  dplyr::select(climbID="animalID", genotype)
+
 
 ## Pull animal metadata from climb based on climbID
-inds <- climbGET(fqf_key$climbID, facet="animals", "animalID") %>%
-  dplyr::select(animalName, "climbID"=animalId, sex, line, dateBorn, origin)
+inds <- climbGET(ID, facet="animals", "animalID") %>%
+  dplyr::select(individualID="animalName", 
+                climbID="animalID", 
+                animalName, 
+                birthID="birthId", 
+                matingID="parentMatingId", 
+                sex, 
+                species,
+                generation,
+                dateBirth="dateBorn",
+                stockNumber="line")
 
-## Pull genotypes from climb based on climbID
-gts <- climbGET(fqf_key$climbID, facet="genotypes", "animalID") %>%
-  dplyr::select("climbID"=animalID, genotype)
+CLIMB_df = merge(inds, gts, by = "climbID")
+
+CLIMB_df['individualIdSource'] = 'UCI_TMF'
+CLIMB_df['materialOrigin'] = 'UCI_TMF'
+CLIMB_df['ageDeathUnits'] = 'months'
+CLIMB_df['rodentDiet'] = '7% standard'
+CLIMB_df['genotypeBackground'] = 'C57BL6J'
+
+CLIMB_df['microchipID'] = ''
+CLIMB_df['ageDeath'] = ''
+CLIMB_df['ageDeathUnits'] = ''
+CLIMB_df['brainWeight'] = ''
+CLIMB_df['rodentWeight'] = ''
+CLIMB_df['bedding'] = ''
+CLIMB_df['room'] = ''
+CLIMB_df['waterpH'] = ''
+CLIMB_df['treatmentDose'] = ''
+CLIMB_df['treatmentType'] = ''
+CLIMB_df['genotypeBackground'] = ''
+CLIMB_df['modelSystemName'] = ''
+CLIMB_df['officialName'] = ''
+
+
+CLIMB_df <- CLIMB_df[, c("individualID" ,
+                         "climbID",
+                         "microchipID",
+                         "birthID",
+                         "matingID",
+                         "individualIdSource",
+                         "materialOrigin",
+                         "sex",
+                         "species",
+                         "generation",
+                         "dateBirth",
+                         "ageDeath",
+                         "ageDeathUnits",
+                         "brainWeight",
+                         "rodentWeight",
+                         "rodentDiet",
+                         "bedding",
+                         "room",
+                         "waterpH",
+                         "treatmentDose",
+                         "treatmentType",
+                         "stockNumber",
+                         "genotype",
+                         "genotypeBackground",
+                         #"individualCommonGenotype"
+                         "modelSystemName",
+                         "officialName")]
+
+write.csv(CLIMB_df, "CLIMB.csv")
+
